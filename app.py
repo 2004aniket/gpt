246838ed8c3d4d2c476ee80a77edafbfdd64428c
Aniket_Pise <118109413+2004aniket@users.noreply.github.com>
2024-08-16 18:23:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+
+
+import streamlit as st
+import os
+
+os.environ["MISTRAL_API_KEY"] = 'kgYDKvLmZ6tPlU2CyFFY2S642npGBHEM'
+
+from langchain_mistralai import ChatMistralAI
+from langchain import LLMChain,PromptTemplate
+st.title("prototype")
+model = ChatMistralAI(model="mistral-large-latest")
+prompt=st.text_input("enter the text")
+template = ''' 
+          CONTEXT: {scenario}
+          STORY: 
+  '''
+
+prompttemp = PromptTemplate (template = template, input_variables = ['scenario'])
+
+if prompt:
+    req=LLMChain(llm=model,prompt=prompttemp)
+    response=req.invoke(prompt)
+    st.write(response)
